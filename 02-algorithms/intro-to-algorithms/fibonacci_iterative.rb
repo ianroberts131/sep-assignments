90955a01907f75f25d6d490bedd467dca1c887f4
ianroberts131 <ianroberts131@gmail.com>
2017-01-29 22:26:04 +0000

    Intro to algorithms solutions

---
+++ 02-algorithms/intro-to-algorithms/fibonacci_iterative.rb
@@ -0,0 +1,28 @@
+def fib(n)
+  return 0 if n == 0
+  return 1 if n == 1
+  
+  prev_prev = 0
+  prev = 1
+  result = 0
+  
+  (2..n).each do |i|
+    result = prev_prev + prev
+    prev_prev = prev
+    prev = result
+  end
+  
+  return result
+  
+end
+
+puts "Fib(0) is #{fib(0)}"
+puts "Fib(1) is #{fib(1)}"
+puts "Fib(2) is #{fib(2)}"
+puts "Fib(3) is #{fib(3)}"
+puts "Fib(4) is #{fib(4)}"
+puts "Fib(5) is #{fib(5)}"
+puts "Fib(6) is #{fib(6)}"
+puts "Fib(7) is #{fib(7)}"
+puts "Fib(8) is #{fib(8)}"
+puts "Fib(9) is #{fib(9)}"
